474dec684aa10118bc6d5f3978c871a38c01ecb9
Mher DeLight <mahirzamil2012@gmail.com>
2026-08-23 12:25:27 +0300

    print function implementations

--- include/IrPrinter.h
+++ include/IrPrinter.h
@@ -6,9 +6,12 @@ namespace casmlang {
 class IrPrinter {
 private:
     std::ostream& stream;
+    std::vector<std::unique_ptr<Ir>> ir;
     int indent = 0;
     void print_indent();
-    void dispatch_print(Ir* instr);
+    void dispatch_print(Ir* instr); // yes, yes, a dispatcher function isn't the best practice but
+                                    // like who really cares it's just a bunch of if statements, way
+                                    // more readable than operator overloads
 
     void instr_print(IrMovStmt* ins);
     void instr_print(IrLabelStmt* ins);
@@ -19,7 +22,7 @@ private:
 public:
     IrPrinter(std::ostream& stream_) : stream(stream_) {}
 
-    void load_ir(std::vector<std::unique_ptr<Ir>> ir);
+    void load_ir(std::vector<std::unique_ptr<Ir>> ir_);
     void print();
 };
 } // namespace casmlang
--- src/IrPrinter.cpp
+++ src/IrPrinter.cpp
@@ -6,4 +6,57 @@ void IrPrinter::print_indent() {
         stream << "    ";
     }
 }
+void IrPrinter::dispatch_print(Ir* instr) {
+    if (!instr)
+        panic("Internal, cannot choose print dispatcher for instruction 0x00");
+
+    if (auto cst = dynamic_cast<IrMovStmt*>(instr))
+        instr_print(cst);
+    else if (auto cst = dynamic_cast<IrLabelStmt*>(instr))
+        instr_print(cst);
+    else if (auto cst = dynamic_cast<IrRetStmt*>(instr))
+        instr_print(cst);
+    else if (auto cst = dynamic_cast<IrRtnCall*>(instr))
+        instr_print(cst);
+    else if (auto cst = dynamic_cast<IrGlobal*>(instr))
+        instr_print(cst);
+    else
+        panic("Internal, invalid print dispatch for instruction");
+}
+
+void IrPrinter::instr_print(IrMovStmt* ins) {
+    print_indent();
+    stream << "mov ";
+    stream << ins->memname;
+    stream << ", ";
+    dispatch_print(ins->value.get());
+}
+void IrPrinter::instr_print(IrLabelStmt* ins) {
+    print_indent();
+    stream << ins->labeltext << ":";
+    indent++;
+}
+void IrPrinter::instr_print(IrRetStmt* ins) {
+    print_indent();
+    stream << "ret";
+    indent--;
+}
+void IrPrinter::instr_print(IrRtnCall* ins) {
+    print_indent();
+    stream << "call " << ins->rtnname;
+}
+void IrPrinter::instr_print(IrGlobal* ins) {
+    print_indent();
+    stream << "global " << ins->identifier;
+}
+
+void IrPrinter::load_ir(std::vector<std::unique_ptr<Ir>> ir_) {
+    ir = std::move(ir_);
+}
+void IrPrinter::print() {
+    for (auto& instr : ir) {
+        dispatch_print(instr.get());
+        stream << "\n";
+    }
+}
 } // namespace casmlang
